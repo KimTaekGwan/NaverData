d517603c271c91d40ef003562593caab7ce5173a
KimTaekGwan <sbe07032@naver.com>
2023-05-15 01:22:11 +0900

    ktg_0515

--- ktg/topKeyword.py
+++ ktg/topKeyword.py
@@ -12,6 +12,8 @@ class TopKeyword:
         self.service = None
         self.driver = None
         self.xpath_dict = {}
+        self.setting()
+        self.xpath_set()
 
     def setting(self):
         self.options = Options()
@@ -46,11 +48,11 @@ class TopKeyword:
         self.driver.get(
             'https://datalab.naver.com/shoppingInsight/sCategory.naver')
 
-        for key in self.xpath_dict.keys()[:9]:
+        for key in list(self.xpath_dict.keys())[:9]:
             xpath = self.xpath_dict[key]
             self._click(xpath=xpath)
 
-        for xpath in self.xpath_dict[key]['age']:
+        for xpath in self.xpath_dict['age']:
             self._click(xpath=xpath)
 
         xpath = self.xpath_dict['search']
@@ -66,14 +68,22 @@ class TopKeyword:
         # print(ls[0])
 
         for _ in range(24):
-            # 다음 페이지 이동
-            element = self.driver.find_element(By.XPATH, , self.xpath_dict['next'])
-            element.click()
             time.sleep(1)
+            # 다음 페이지 이동
+            element = self.driver.find_element(
+                By.XPATH, self.xpath_dict['next'])
+            element.click()
 
             # 인기 검색어 리스트 20개 가져오기
-            element = self.driver.find_element(By.XPATH, self.xpath_dict['top20'])
+            element = self.driver.find_element(
+                By.XPATH, self.xpath_dict['top20'])
             data = element.text.split('\n')
             ls = list(zip(data[0::2], data[1::2]))
             total_ls += ls
             # print(ls[0])
+        return total_ls
+
+
+if __name__ == '__main__':
+    py = TopKeyword()
+    py.run()
